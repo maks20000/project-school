d9d1e2395d4e2603a6083f985102016e9d392600
Maksim <maks20000@gmail.com>
2019-11-25 13:07:04 +0300

    37

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,10 @@ $(function () {
     let hide_menu = $(".hide-menu");
     hide_menu.on('click',function() {
         var target = $(this).attr("target");
-        $("#"+target).toggle();
+        $(openned).toggle();
+        if (openned!=="#"+target) {
+            $("#"+target).toggle();
+            openned="#"+target;
+        } else openned=undefined;
     })
 });
